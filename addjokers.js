06126eb60e46319e6ef12488e650e8adacd6f46d
not mario <59957544+notmario@users.noreply.github.com>
2024-03-09 21:03:49 +0800

    Initial commit

---
+++ addjokers.js
@@ -0,0 +1,133 @@
+let jokers = [
+  {
+    name: "Joker",
+    text: [
+      "{C:mult}+4{} Mult"
+    ],
+    image_url: "img/j_joker.png",
+    rarity: "Common"
+  }
+]
+
+let cols = {
+  
+  MULT: "#FE5F55",
+  CHIPS: "#009dff",
+  MONEY: "#f3b958",
+  XMULT: "#FE5F55",
+  FILTER: "#ff9a00",
+  ATTENTION: "#ff9a00",
+  BLUE: "#009dff",
+  RED: "#FE5F55",
+  GREEN: "#4BC292",
+  PALE_GREEN: "#56a887",
+  ORANGE: "#fda200",
+  IMPORTANT: "#ff9a00",
+  GOLD: "#eac058",
+  YELLOW: "#ffff00",
+  CLEAR: "#00000000", 
+  WHITE: "#ffffff",
+  PURPLE: "#8867a5",
+  BLACK: "#374244",
+  L_BLACK: "#4f6367",
+  GREY: "#5f7377",
+  CHANCE: "#4BC292",
+  JOKER_GREY: "#bfc7d5",
+  VOUCHER: "#cb724c",
+  BOOSTER: "#646eb7",
+  EDITION: "#ffffff",
+  DARK_EDITION: "#000000",
+  ETERNAL: "#c75985",
+  DYN_UI: {
+    MAIN: "#374244",
+    DARK: "#374244",
+    BOSS_MAIN: "#374244",
+    BOSS_DARK: "#374244",
+    BOSS_PALE: "#374244"
+  },
+  SO_1: {
+    Hearts: "#f03464",
+    Diamonds: "#f06b3f",
+    Spades: "#403995",
+    Clubs: "#235955",
+  },
+  SO_2: {
+    Hearts: "#f83b2f",
+    Diamonds: "#e29000",
+    Spades: "#4f31b9",
+    Clubs: "#008ee6",
+  },
+  SUITS: {
+      Hearts: "#FE5F55",
+      Diamonds: "#FE5F55",
+      Spades: "#374649",
+      Clubs: "#424e54",
+  },
+  
+  SET: {
+    Default: "#cdd9dc",
+    Enhanced: "#cdd9dc",
+    Joker: "#424e54",
+    Tarot: "#424e54",
+    Planet: "#424e54",
+    Spectral: "#424e54",
+    Voucher: "#424e54",
+  }, 
+  SECONDARY_SET: {
+    Default: "#9bb6bdFF",
+    Enhanced: "#8389DDFF",
+    Joker: "#708b91",
+    Tarot: "#a782d1",
+    Planet: "#13afce",
+    Spectral: "#4584fa",
+    Voucher: "#fd682b",
+    Edition: "#4ca893",
+  },
+}
+
+let rarities = {
+  "Common": "#009dff", 
+  "Uncommon": "#4BC292",
+  "Rare": "#fe5f55",
+  "Legendary": "#b26cbb"
+}
+
+regex = /{([^}]+)}/g;
+
+let jokers_div = document.querySelector(".jokers");
+
+for (let joker of jokers) {
+  console.log("adding joker", joker.name);
+
+  joker.text = joker.text.map((line) => { return line + "{}"});
+
+  joker.text = joker.text.join("<br/>");
+  joker.text = joker.text.replaceAll("{}", "</span>");
+  joker.text = joker.text.replace(regex, function replacer(match, p1, offset, string, groups) {
+    let classes = p1.split(",");
+
+    let css_styling = "";
+
+    for (let i = 0; i < classes.length; i++) {
+      let parts = classes[i].split(":");
+      if (parts[0] === "C") {
+        css_styling += `color: ${cols[parts[1].toUpperCase()]};`;
+      } else if (parts[0] === "X") {
+        css_styling += `background-color: ${cols[parts[1].toUpperCase()]}; border-radius: 5px; padding: 0 5px;`;
+      }
+    }
+
+    return `</span><span style='${css_styling}'>`;
+  });
+
+  let joker_div = document.createElement("div");
+  joker_div.classList.add("joker");
+  joker_div.innerHTML = `
+    <h3>${joker.name}</h3>
+    <img src="${joker.image_url}" alt="${joker.name}" />
+    <h4 class="rarity" style="background-color: ${rarities[joker.rarity]}">${joker.rarity}</h4>
+    <div class="text">${joker.text}</div>
+  `;
+
+  jokers_div.appendChild(joker_div);
+}
